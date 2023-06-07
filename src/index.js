08f3568b4aa7052809f47dcb6cfdacf2103c7cc5
krk224 <hihas93@naver.com>
2023-06-07 12:50:29 +0900

    release13 브랜치 기능 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import release13 from './release/release13.js';
+
 console.log('Hello main branch');
 console.log('release/0.1.13 branch is added');
+
+release13();
---
+++ src/release/release13.js
@@ -0,0 +1,5 @@
+const release13 = () => {
+  console.log('release13');
+};
+
+export default release13;
