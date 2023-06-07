303590d4816ca83f65f9fbb57d5ded307293e340
krk224 <hihas93@naver.com>
2023-06-07 12:52:54 +0900

    release0.1.4 기능 구현 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import release13 from './release/release13.js';
+import release14 from './release/release14.js';
 
 console.log('Hello main branch');
 console.log('release/0.1.13 branch is added');
 
 release13();
+release14();
---
+++ src/release/release14.js
@@ -0,0 +1,5 @@
+const release14 = () => {
+  console.log('release14');
+};
+
+export default release14;
